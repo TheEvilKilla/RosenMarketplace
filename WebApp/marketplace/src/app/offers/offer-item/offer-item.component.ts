b5e132e7ca5f19bea2bd9dcdb14c74d26271c29c
Carlos Robles <carlos2705r2@hotmail.com>
2024-02-25 23:31:13 -0500

    Card component done

--- WebApp/marketplace/src/app/offers/offer-item/offer-item.component.ts
+++ WebApp/marketplace/src/app/offers/offer-item/offer-item.component.ts
@@ -1,5 +1,5 @@
-import {Component, Input, OnInit} from '@angular/core';
-import {OfferModel} from '../../core/marketplace-api/models/offer.model';
+import { Component, Input, OnInit } from '@angular/core';
+import { OfferModel } from '../../core/marketplace-api/models/offer.model';
 
 @Component({
   selector: 'app-offer-item',
@@ -10,8 +10,13 @@ export class OfferItemComponent implements OnInit {
 
   @Input()
   offer: OfferModel;
+  title: string = 'Puff Pera Lona Impermeable Naranja';
+  details: string = 'CarlosRobles 10/01/2024  14:00';
+  description: string = "Esto es la descripcion de algo";
 
-  constructor() { }
+  constructor() { 
+    // this.title = this.offer.title;
+  }
 
   ngOnInit(): void {
   }
